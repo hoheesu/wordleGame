52b96f7824c35c1fbd99adf3d2bf15ddef753a18
hoheesu <hoheesu@naver.com>
2023-10-03 20:33:50 +0900

    Fix: 배포 오류

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import topLevelAwait from "vite-plugin-top-level-await";
+// import topLevelAwait from "vite-plugin-top-level-await";
 
-export default {
-  plugins: [
-    topLevelAwait({
-      // The export name of top-level await promise for each chunk module
-      promiseExportName: "__tla",
-      // The function to generate import names of top-level await promise in each chunk module
-      promiseImportName: (i) => `__tla_${i}`,
-    }),
-  ],
-  build: {
-    target: ["edge90", "chrome90", "firefox90", "safari15"],
-  },
-};
+// export default {
+//   plugins: [
+//     topLevelAwait({
+//       // The export name of top-level await promise for each chunk module
+//       promiseExportName: "__tla",
+//       // The function to generate import names of top-level await promise in each chunk module
+//       promiseImportName: (i) => `__tla_${i}`,
+//     }),
+//   ],
+//   build: {
+//     target: ["edge90", "chrome90", "firefox90", "safari15"],
+//   },
+// };
