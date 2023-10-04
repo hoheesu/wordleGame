8e67303b2673b981764ad5a13a9f6922bc976654
hoheesu <hoheesu@naver.com>
2023-10-04 19:42:03 +0900

    Fix: 배포 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import VitePluginSass from "vite-plugin-sass";
+// import { defineConfig } from "vite";
+// import VitePluginSass from "vite-plugin-sass";
 
-export default defineConfig({
-  plugins: [VitePluginSass()],
-});
+// export default defineConfig({
+//   plugins: [VitePluginSass()],
+// });
