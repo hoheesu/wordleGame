124a7de8f0cc3cfb5b300b55acac9c0124e18e87
hoheesu <hoheesu@naver.com>
2023-10-05 22:42:08 +0900

    Fix: aixos 에러

--- src/script/axios.ts
+++ src/script/axios.ts
@@ -1,15 +1,33 @@
-// import axios from "axios";
-import axios from "/path/to/axios.js"; // 절대 경로 사용
+// // import axios from "axios";
+// import axios from "axios"; // 절대 경로 사용
+
+// const BASE_URL = "https://random-word.ryanrk.com";
+// export const getWord = async () => {
+//   try {
+//     const response = await axios.get(
+//       `${BASE_URL}/api/en/word/random/?length=5`,
+//     );
+//     console.log(response.data[0]);
+//     return response.data[0];
+//   } catch (errors) {
+//     console.error(errors);
+//   }
+// };
 
 const BASE_URL = "https://random-word.ryanrk.com";
+
 export const getWord = async () => {
   try {
-    const response = await axios.get(
-      `${BASE_URL}/api/en/word/random/?length=5`,
-    );
-    console.log(response.data[0]);
-    return response.data[0];
-  } catch (errors) {
-    console.error(errors);
+    const response = await fetch(`${BASE_URL}/api/en/word/random/?length=5`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data[0]);
+    return data[0];
+  } catch (error) {
+    console.error(error);
   }
 };
